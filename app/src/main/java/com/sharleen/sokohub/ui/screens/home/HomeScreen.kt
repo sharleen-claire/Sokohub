c9e8dc3fe4d3955f8adffd0f35da2032586aa9dd
sharleen-claire <clairesharleen17@gmail.com>
2026-04-02 01:44:38 -0700

    initial commit

--- app/src/main/java/com/sharleen/sokohub/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/sharleen/sokohub/ui/screens/home/HomeScreen.kt
@@ -2,24 +2,69 @@ package com.sharleen.sokohub.ui.screens.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.sharleen.sokohub.ui.theme.Blueberry
 
+@OptIn(ExperimentalMaterial3Api::class)
  @Composable
 fun HomeScreen(){
      Column(
          modifier = Modifier.fillMaxSize()
      ){
 
+         TopAppBar(
+             title = {Text(text = "Home")},
+             navigationIcon = {
+                 IconButton(onClick = {}) {
+                     Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                 }
 
-     }
+
+             },
+             actions = {
+                 IconButton(onClick = {}) {
+                     Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Menu")
+                 }
+                 IconButton(onClick = {}) {
+                     Icon(imageVector = Icons.Default.Notifications, contentDescription = "Menu")
+                 }
+
+             },
+             colors = TopAppBarDefaults.topAppBarColors(
+                 containerColor = Blueberry,
+                 navigationIconContentColor = Color.White,
+             )
+         )
+
+
+
+
+
+
+
+
+             }
+
+
+ }
     
 
 
 
-}
+
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview(){
--- app/src/main/java/com/sharleen/sokohub/ui/screens/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/sharleen/sokohub/ui/screens/onboarding/OnboardingScreen.kt
@@ -1,26 +1,44 @@
 package com.sharleen.sokohub.ui.screens.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sharleen.sokohub.R
 import com.sharleen.sokohub.ui.screens.home.HomeScreen
+import com.sharleen.sokohub.ui.theme.Blueberry
+
 @Composable
 fun OnboardingScreen(){
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
 
         Image(
             painter = painterResource(R.drawable.img),
             contentDescription = "img",
-            modifier = Modifier.size(100.dp)
+            modifier = Modifier.size(300.dp)
 
 
 
@@ -29,6 +47,50 @@ fun OnboardingScreen(){
 
 
         )
+
+        Spacer(modifier = Modifier.height(20.dp) )
+
+        Text(
+            text = "Welcome to Sokohub!!",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif,
+            color = Blueberry
+        )
+        Text(
+            text = "Shop Smarter,",
+            fontSize = 20.sp,
+
+
+
+
+        )
+        Text(
+            text = "Everywhere you go,",
+            fontSize = 20.sp,
+
+
+            )
+
+            Text(
+            text = "Welcome to our store—where quality meets convenience. We’re committed to providing a smooth and enjoyable shopping experience with carefully selected products, secure payments, and fast, reliable delivery you can trust. Our team is always ready to assist you and ensure your satisfaction every step of the way. Thank you for choosing us as your shopping destination, and we look forward to serving you.\n,",
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify
+        )
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Blueberry),
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.width(350.dp)
+
+        ) {
+            Text(text = "Get Started")
+        }
+
+
+
+
+
 
 
 

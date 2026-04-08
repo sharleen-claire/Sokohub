55e39a756402145ea0b4a932292709914caf3779
sharleen-claire <clairesharleen17@gmail.com>
2026-04-08 11:15:46 +0300

    initial commit

---
+++ app/src/main/java/com/sharleen/sokohub/ui/screens/auth/LoginScreen.kt
@@ -0,0 +1,150 @@
+package com.sharleen.sokohub.ui.screens.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.clementvexegon.sokohub.R
+import com.clementvexegon.sokohub.ui.theme.Blueberry
+
+@Composable
+fun LoginScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(painter = painterResource(R.drawable.background), contentScale = ContentScale.FillBounds),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+
+    ){
+
+        Image(
+            painter = painterResource(R.drawable.products),
+            contentDescription = "products",
+            modifier = Modifier.size(200.dp)
+
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Join us and start your journey today",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+
+            )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Variables
+
+        var email by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+
+        //Email
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            modifier = Modifier.width(350.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "") },
+            label = { Text(text = "email")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Blueberry,
+                focusedBorderColor = Color.Black,
+                unfocusedLeadingIconColor = Blueberry,
+                unfocusedLabelColor = Blueberry
+
+
+
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        //Password
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            modifier = Modifier.width(350.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "") },
+            label = { Text(text = "password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = Blueberry,
+                focusedBorderColor = Color.Black,
+                unfocusedLeadingIconColor = Blueberry,
+                unfocusedLabelColor = Blueberry
+
+
+
+            ),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(Blueberry),
+            shape = RoundedCornerShape(10.dp),
+
+            ){
+            Text(text = "Login")
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        Text(text = "Don't have an account? Register",
+            fontSize = 18.sp,
+            color =Blueberry
+        )
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    LoginScreen()
+}
